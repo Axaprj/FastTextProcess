6b026c41a1c20a1906ddf2ee0a1198f2523281f4
Igor Alexeev <axaprj2000@yahoo.com>
2020-05-02 18:49:03 +0300

    ReplaceDemoTest

---
+++ Axaprj.Textc.Vect.Test/ReplaceDemoTest.cs
@@ -0,0 +1,78 @@
+using Axaprj.FastTextProcess;
+using Axaprj.Textc.Vect.Attributes;
+using Axaprj.WordToVecDB.Enums;
+using System.Threading;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Axaprj.Textc.Vect.Test
+{
+    /// <summary>Unit Of Measure</summary>
+    enum UOM
+    {
+        kW,
+        Nm,
+        L,
+        L_100Km,
+        g_Km
+    }
+    /// <summary>Vehicles Features</summary>
+    enum VFeat
+    {
+        [ReplaceTC(
+        SyntaxPattern = ":Word(EU5) :Word?(-) :VWord(compliant)", Lng = LangLabel.en)]
+        EU5compliant,
+        [ReplaceNum(
+            SyntaxPattern =
+            "num:Decimal :Word?(-) :VWord?(litre) :VWord?(turbo) :Word?(-) :VWord(diesel) :VWord(engine)",
+            UOM = UOM.L, Lng = LangLabel.en)]
+        DIESEL_ENG,
+        [ReplaceNum(
+            SyntaxPattern =
+            "num:Decimal :Word(CO2) :VWord?(emission)",
+            UOM = UOM.g_Km, Lng = LangLabel.en)]
+        CO2,
+        [ReplaceNum(
+            SyntaxPatterns = new string[] {
+            "num:Decimal :VWord(liters) :VWord(per) :Word(100) :VWord(kilometers)",
+            "num:Decimal :Word(L) :Word(/) :Word(100) :Word(km)"
+            },
+            UOM = UOM.L_100Km, Lng = LangLabel.en)]
+        FuelConsumption
+    }
+
+    public class ReplaceDemoTest : TestBase
+    {
+        string TestPH1 =
+            $"The EU5-compliant, 130kW/430Nm 3.0-litre turbo-diesel engine introduced in the MY16.5 MU-X(and MY17 D-MAX)"
+            + $" continues on with the option of six-speed manual or six-speed automatic transmissions."
+            + $"The lowest combined fuel consumption for the range is 7.9L/100km for LS-U and LS-T 4x4 autos(209 CO2 emissions),"
+            + $" and peaks at 8.1 liter per 100 km.";
+
+        public ReplaceDemoTest(ITestOutputHelper output) : base(output) { }
+
+        [Fact]
+        public void VehiclesFeaturesTest()
+        {
+            Log(TestPH1);
+            var ptxt = RegExUtil.RegExClean(TestPH1);
+            ptxt = RegExUtil.Prepare(ptxt);
+            Log(ptxt);
+            var ctx = CreateContext(ptxt);
+            ctx.ProcessRepl<VFeat>(CancellationToken.None);
+            Log(ctx.TextProcess);
+        }
+
+        VRequestContext CreateContext(string txt)
+        {
+            AssertFileExists(W2VDictEN);
+            return new VRequestContext()
+            {
+                TextProcess = txt,
+                W2VDictFile = W2VDictEN,
+                LangLabel = LangLabel.en,
+                MinCosine = 0.6f
+            };
+        }
+    }
+}
--- Axaprj.Textc.Vect/RegExUtil.cs
+++ Axaprj.Textc.Vect/RegExUtil.cs
@@ -4,6 +4,17 @@ namespace Axaprj.Textc.Vect
 {
     public static class RegExUtil
     {
+        static readonly Regex rexClnSpaces = new Regex("\\s{2,}", RegexOptions.Compiled);
+        static readonly Regex rexClnCommonEn = 
+            new Regex("[^A-Za-z0-9(),.!?\'`\"/-]", RegexOptions.Compiled);
+        static readonly Regex rexClnCommonEnCyr = 
+            new Regex("[^A-Za-zА-Яа-яІіЇїЄєҐґЁё0-9(),.!?\'`\"/-]", RegexOptions.Compiled);
+        static readonly Regex rexVe = new Regex("(?<lett>[A-Za-z]) ' ve", RegexOptions.Compiled);
+        static readonly Regex rexRe = new Regex("(?<lett>[A-Za-z]) ' re", RegexOptions.Compiled);
+        static readonly Regex rexD = new Regex("(?<lett>[A-Za-z]) ' d", RegexOptions.Compiled);
+        static readonly Regex rexLL = new Regex("(?<lett>[A-Za-z]) ' ll", RegexOptions.Compiled);
+        static readonly Regex rexS = new Regex("(?<lett>[A-Za-z]) ' s", RegexOptions.Compiled);
+
         static readonly string SP = " ";
         static readonly Regex rexNumSp = new Regex(
             @" [0-9]*(?: \. [0-9]*)? ", RegexOptions.Compiled);
@@ -21,5 +32,35 @@ namespace Axaprj.Textc.Vect
                 (m) => SP + m.Value.Replace(SP, string.Empty) + SP);
             return res;
         }
+        
+        static public string RegExClean(string src) =>
+            CleanCommon(src, rexClnCommonEnCyr);
+
+        static string CleanCommon(string str, Regex clean_rex)
+        {
+            //str = str.ToLower();
+            str = clean_rex.Replace(str, " ");
+            str = str
+                .Replace("-", " - ") // add
+                .Replace("/", " / ") // add
+                .Replace("'", " ' ") // add
+                .Replace("\"", " \" ") // add
+                .Replace("`", " ' ") // add
+                .Replace(".", " . ") // add
+                .Replace(",", " , ")
+                .Replace("!", " ! ")
+                .Replace("(", " ( ")
+                .Replace(")", " ) ")
+                .Replace("?", " ? ");
+            str = str.Replace("n ' t", "n't");
+            str = rexVe.Replace(str, "${lett} 've");
+            str = rexRe.Replace(str, "${lett} 're");
+            str = rexD.Replace(str, "${lett} 'd");
+            str = rexLL.Replace(str, "${lett} 'll");
+            str = rexS.Replace(str, "${lett} 's");
+            str = rexClnSpaces.Replace(str, " ");
+            return str.Trim();
+        }
+
     }
 }
